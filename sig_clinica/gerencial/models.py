7e3c21677294176f06bc622d4d30bc7f169380f0
Cris <dchrisviii@gmail.com>
2019-06-18 12:46:55 -0600

    Actualizacion modelo

--- sig_clinica/gerencial/models.py
+++ sig_clinica/gerencial/models.py
@@ -30,8 +30,9 @@ class Medicamento(models.Model):
  
 class LoteMedicamento(models.Model):
     medicamento = models.ForeignKey(Medicamento, blank = False, null = False,on_delete = models.CASCADE)
-    fecha_vencimiento = models.DateField('Fecha de Vencimiento', help_text = 'Formato: DD/MM/AAAA',blank = False, null = False)
-    cantidad=models.IntegerField('Cantidad vencida',blank=False,null=False,validators = [MinValueValidator(0)])
+    fecha_vencimiento = models.DateField('Fecha de Vencimiento',blank = False, null = False)
+    fecha_entrada = models.DateField('Fecha de entrada',blank = False, null = False , default=tz.now)
+    cantidad=models.IntegerField('Cantidad de entrada',blank=False,null=False,validators = [MinValueValidator(0)])
 
 
 class Odontograma(models.Model):
@@ -51,13 +52,8 @@ class Paciente(models.Model):
     )
     nombresPaciente = models.CharField('Nombres del paciente', max_length=60, blank=False, null=False)
     apellidosPaciente = models.CharField(max_length=60, blank=False, null=False)
-    ocupacion = models.CharField(max_length=60, blank=True, null=True)
     sexo = models.CharField(max_length=2, choices=SEXO_CHOICES, default=None, blank=False, null=False)
-    fechaNacimiento = models.DateField('Fecha de nacimiento', help_text='Formato: DD-MM-AAAA', blank=False, null=False)
-    direccionCasa = models.CharField('Direccion Casa', max_length=150, blank=True, null=True)
-    direccionTrabajo = models.CharField('Direccion Trabajo', max_length=150, blank=True, null=True)
-    telefonoCasa = models.CharField('Telefono Casa', max_length=9, help_text='Formato: XXXX-XXXX', blank=True, null=True)
-    telefonoTrabajo = models.CharField('Telefono Trabajo', max_length=9, help_text='Formato: XXXX-XXXX', blank=True, null=True)
+    fechaNacimiento = models.DateField('Fecha de nacimiento', blank=False, null=False)
     referencia = models.CharField('Responsable', max_length=60, help_text='(En caso de ser niño)', blank=True, null=True)
 
     def __str__(self):
@@ -87,21 +83,12 @@ class Expediente(models.Model):
         verbose_name_plural = 'Expedientes'
 
 class Cita(models.Model):
-    APLICADA = 'Aplicada'
-    PENDIENTE = "Pendiente"
-    NO_ASISTIO = 'No Asistio'
-    ESTADO_CHOICES = (
-        (APLICADA, 'Aplicada'),
-        (PENDIENTE, 'Pendiente'),
-        (NO_ASISTIO,'No Asistio'),
-        )
     asuntoCita=models.CharField('Asunto de la cita', max_length=50,blank=False,null=False)
     paciente = models.ForeignKey(Expediente, on_delete=models.PROTECT)
     doctor = models.ForeignKey(Doctor, on_delete=models.PROTECT)
-    fechaCita = models.DateField('Fecha de Cita', help_text='Formato: AAAA-MM-DD', blank=False, null=False)
+    fechaCita = models.DateField('Fecha de Cita', blank=False, null=False)
     horaCita = models.TimeField('Hora de Cita', blank=False, null=False)
     observacionCita = models.TextField('Observaciones', max_length=250, blank=True, null=True)
-    estado = models.CharField(max_length=10, choices=ESTADO_CHOICES, default=None, blank=False, null=False)
 
 
     def __str__(self):
@@ -182,8 +169,12 @@ class Pago(models.Model):
         return '#%s - Pago por: %s del %s' % (self.id, self.cantidad ,self.Expediente)
 
 class Receta(models.Model):
+    nombreReceta = models.CharField('Nombre de la receta', max_length=100, blank=False, null=False, default='Receta')
     consulta = models.ForeignKey('Consulta', on_delete=models.PROTECT)
     medicamento = models.ManyToManyField(Medicamento, through='Especificacion')
+
+    def __str__(self):
+        return self.nombreReceta
 
 
 class Especificacion(models.Model):
