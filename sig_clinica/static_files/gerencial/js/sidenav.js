4854a7b4270662adecb6fba88fc4ba628c5d6ccc
Carlos Cativo <cativo23.kt@gmail.com>
2019-06-30 16:10:17 -0600

    asyuda

--- sig_clinica/static_files/gerencial/js/sidenav.js
+++ sig_clinica/static_files/gerencial/js/sidenav.js
@@ -12,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
-
     });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
   });
